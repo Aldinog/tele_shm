0412a60fac1d8c3164d5173bd883a3e298ce01a9
Aldino satria <satriaaldino96@gmail.com>
2025-11-30 17:02:45 +0700

    Update project

--- utils/harga.js
+++ utils/harga.js
@@ -2,12 +2,16 @@ const axios = require('axios');
 
 async function getHargaSaham(kode) {
   try {
-    const url = `${process.env.GOAPI_URL}/${kode}?api_key=${process.env.GOAPI_KEY}`;
+    const url = `${process.env.GOAPI_URL}?simbol=${kode}&api_key=${process.env.GOAPI_KEY}`;
+    // Contoh URL final:
+    // https://api.goapi.io/saham/idx/harga?simbol=BBCA&api_key=xxxxxxxx
+
     const response = await axios.get(url);
 
     if (!response.data || !response.data.data) {
       return null;
     }
+
     return response.data.data;
   } catch (error) {
     console.error("Error API:", error.response?.data || error);
