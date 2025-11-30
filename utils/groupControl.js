cc606941be5af2a43059ed6a8018506a03a010dd
Aldino satria <satriaaldino96@gmail.com>
2025-11-30 13:36:52 +0700

    Update project

--- utils/groupControl.js
+++ utils/groupControl.js
@@ -1,9 +1,9 @@
-// const allowedGroups = process.env.ALLOWED_GROUPS
-//   ? process.env.ALLOWED_GROUPS.split(",").map(id => id.trim())
-//   : [];
+const allowedGroups = process.env.ALLOWED_GROUPS
+  ? process.env.ALLOWED_GROUPS.split(",").map(id => id.trim())
+  : [];
 
-// function isAllowedGroup(chatId) {
-//   return allowedGroups.includes(chatId.toString());
-// }
+function isAllowedGroup(chatId) {
+  return allowedGroups.includes(chatId.toString());
+}
 
-// module.exports = { isAllowedGroup };
+module.exports = { isAllowedGroup };
